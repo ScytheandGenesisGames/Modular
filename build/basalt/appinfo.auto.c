8ba8108fc001e87480ee54ab3cb46c4dfae7571b
Dr. Alexander <lieutenantcommander.christopher@gmail.com>
2020-02-09 18:59:53 -0500

    Build 1

---
+++ build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Apple Watch Modular",
+  .company = "Dr Alexander the bat",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_BASALT,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x80, 0x88, 0x13, 0xB8, 0x43, 0x16, 0x43, 0x9D, 0xA7, 0x0B, 0xFF, 0x76, 0xC0, 0x24, 0x4C, 0xCF },
+  .virtual_size = 0xb6b6
+};
